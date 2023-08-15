a4e17a138c3a85d9e3bdc05cfdb69c116dea7e2c
Gaurav <gangulygaurav166@gmail.com>
2023-08-15 22:44:51 +0530

    zomato Chronicles with frontend

---
+++ Zomato Chronicles with Frontend/Backend/zomato/zomcrud/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('menu/', views.menu_list, name='menu_list'),
+    path('dish/add', views.add_dish, name='add_dish'),
+    path('dish/remove', views.remove_dish, name='remove_dish'),   #query
+    path('dish/update', views.update_dish, name='update_dish'),   #query
+    path('order/', views.order_list, name='order_list'),
+    path('order/takeorder', views.take_order, name='take_order'),
+    path('order/update', views.update_order_status, name='update_order_status'),  #query
+]
