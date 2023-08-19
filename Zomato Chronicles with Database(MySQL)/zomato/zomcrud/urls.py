f381c01fc527e7f4303afee63be47911866ca54f
Gaurav <gangulygaurav166@gmail.com>
2023-08-19 22:14:09 +0530

    Zomato Chronicles with Database Completed

--- Zomato Chronicles with Database(MySQL)/zomato/zomcrud/urls.py
+++ Zomato Chronicles with Database(MySQL)/zomato/zomcrud/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('add-dish', add_dish, name='add_dish'),
     path('delete-dish/<int:dishID>', delete_dish, name='delete_dish'),
     path('update-availability/<int:dishID>/<str:is_available>', update_availability, name='update_availability'),
+    path('update-dish/<int:dishID>', update_dish, name='update_dish'),
+    path('order-items', order_items, name='order_items'),
+    path('orders', orders, name='orders'),
+    path('order-details/<int:orderID>', order_details, name='order_details')
 ]
 
 if settings.DEBUG:
--- Zomato Chronicles with Database(MySQL)/zomato/zomcrud/views.py
+++ Zomato Chronicles with Database(MySQL)/zomato/zomcrud/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .models import Dishes, Orders
 
@@ -5,14 +6,33 @@ from .models import Dishes, Orders
 def home(request):
     return render(request, 'base.html')
 
+def search(request, page):
+    
+    dishes = Dishes.objects.filter(name__icontains = request.GET.get('search'))
+    
+    if len(dishes) == 0:
+        return None
+    else:
+        return render(request, page+'.html', context={'dishes': dishes})
+
 def menu(request):
     
     dishes = Dishes.objects.all()
     
-    return render(request, 'menu.html', context={'dishes': dishes})
+    if request.GET.get('search'):
+        search_results = search(request, 'menu')
+        if search_results is not None:
+            return search_results
+    
+    else:
+        return render(request, 'menu.html', context={'dishes': dishes})
 
 
 def add_dish(request):
+    if request.GET.get('search'):
+        search_results = search(request, 'menu')
+        if search_results is not None:
+            return search_results
     
     if request.method == "POST":
         
@@ -33,6 +53,7 @@ def add_dish(request):
         
         return redirect('/menu')
         # If we don't redirect it, it will try to persist data to the database when doing hard refresh.
+        
     
     return render(request, "add_dish.html")
 
@@ -49,3 +70,102 @@ def update_availability(request, dishID, is_available):
     dish = Dishes.objects.filter(dishID = dishID).update(is_available = False if is_available == 'True' else True)
     
     return redirect('/menu')
+
+
+def update_dish(request, dishID):
+    
+    if request.GET.get('search'):
+        search_results = search(request, 'menu')
+        if search_results is not None:
+            return search_results
+    
+    dish = Dishes.objects.get(dishID = dishID)
+    
+    if request.method == "POST":
+        
+        data = request.POST
+        name = data.get('name')
+        description = data.get('description')
+        price = data.get('price')
+        image = request.FILES.get('image')
+        is_available = data.get('is_available') == 'on'
+        
+        dish.name = name
+        dish.description = description
+        dish.price = price
+        dish.is_available = is_available
+        
+        if image is not None:
+            dish.image = image
+            
+        dish.save()
+            
+        return redirect('/menu')
+        
+    return render(request, 'update_dish.html', context={'dish': dish})
+    
+    
+    
+    
+def order_items(request):
+    
+    if request.GET.get('search'):
+        search_results = search(request, 'order_items')
+        if search_results is not None:
+            return search_results
+    
+    if request.method == 'POST':
+        # Get the selected dish IDs from the form
+        selected_dish_ids = request.POST.getlist('selected_dishes')
+        customer_name = request.POST.get('customer_name')
+        
+        # Query the database to check if all selected dishes are available
+        available_dishes = Dishes.objects.filter(dishID__in=selected_dish_ids, is_available=True)
+        
+        if available_dishes.count() == len(selected_dish_ids):
+            # All selected dishes are available, proceed with the order
+            
+            # Calculate the total bill based on the selected dishes' prices
+            total_bill = sum(dish.price for dish in available_dishes)
+
+            # Create an Order instance
+            order = Orders.objects.create(
+                customer_name = customer_name,
+                total_bill = total_bill
+            )
+
+            # Add the selected dishes to the order
+            order.dishes.set(available_dishes)
+            
+            return redirect('orders')
+        
+        else:
+            # Not all selected dishes are available
+            # You can handle this case accordingly, for example, by showing an error message
+            return JsonResponse({"Error": 'Some Dishes are not available, please order carefully!!'}, status=400)
+        
+
+    dishes = Dishes.objects.all()
+    return render(request, 'order_items.html', {'dishes': dishes})
+    
+
+def orders(request):
+    
+    orders = Orders.objects.all()
+    
+    return render(request, 'orders.html', context={"orders": orders})
+
+
+def order_details(request, orderID):
+    
+    order = Orders.objects.get(orderID = orderID)
+    print(order)
+    
+    if order:
+        dishes = order.dishes.all()
+        print(dishes)
+        
+        return render(request, 'order_details.html', context={"dishes": dishes})
+    
+    else:
+        return JsonResponse({"Error": 'Wrong orderID entered'}, status=400)
